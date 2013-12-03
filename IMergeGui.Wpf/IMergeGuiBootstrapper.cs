b77775959e8d56c04dcfd0b7c15405e7db6d890a
Ben Straub <bs@github.com>
2013-12-03 08:26:04 -0800

    Add Caliburn.Micro bootstrapper, shell VM and view

---
+++ IMergeGui.Wpf/IMergeGuiBootstrapper.cs
@@ -0,0 +1,18 @@
+﻿using Caliburn.Micro;
+
+namespace IMergeGui.Wpf
+{
+    public class IMergeGuiBootstrapper : Bootstrapper<Core.ShellViewModel>
+    {
+        public IMergeGuiBootstrapper()
+        {
+            var mappingConfig = new TypeMappingConfiguration
+            {
+                DefaultSubNamespaceForViewModels = "Core",
+                DefaultSubNamespaceForViews = "Wpf",
+            };
+            ViewLocator.ConfigureTypeMappings(mappingConfig);
+            ViewModelLocator.ConfigureTypeMappings(mappingConfig);
+        }
+    }
+}
